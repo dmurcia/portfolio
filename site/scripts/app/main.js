36f8e47cc2ba6f71a7b503dea47aef237b0f0b82
David <dmurcia@outlook.com>
2014-07-28 17:09:30 -0600

    Start mobile version

--- site/scripts/app/main.js
+++ site/scripts/app/main.js
@@ -3,10 +3,4 @@ define(function (require) {
 	// Load modules with require call 
 	var navigation = require('./navigation');
 
-	// DOM variables
-	var nav = $('nav');
-
-	nav.on('click', navigation.goTo());	
-	
-
 });
